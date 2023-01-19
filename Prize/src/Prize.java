d3d15600dadd926e62f5e604f45463088c4a5f2f
LENOVO <LENOVO@192.168.0.3>
2023-01-19 17:14:33 +0530

    add

---
+++ Prize/src/Prize.java
@@ -0,0 +1,27 @@
+
+public class Prize {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+		int z;
+		int total = x+y+z;
+		if (total==180);{
+			if (x==y||y==z||x==z);{
+			System.out.println("prize 3");
+			}
+			if (x==90||y==90||z==90);{
+			System.out.println("prize 2");
+			}
+		}
+			else {
+			System.out.println("prize1");
+			}
+		
+			if (total!=180);
+				System.out.print("No Prize");
+		}
+
+	}
+
+
